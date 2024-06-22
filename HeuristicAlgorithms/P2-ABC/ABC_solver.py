fc8b65bc6183163abc7490c859e29f7d9638d816
MinuxAE <siliconmagic@aliyun.com>
2024-06-22 08:39:14 +0800

    ABC Solver

--- HeuristicAlgorithms/P2-ABC/ABC_solver.py
+++ HeuristicAlgorithms/P2-ABC/ABC_solver.py
@@ -6,3 +6,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 import LibABC
 
+# 适应度函数
+def fun(X):
+    return X[0]**2 + X[1]**2
+
+def vis():
+    fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
+    
+    x1 = np.arange(-10, 10, 0.2)
+    x2 = np.arange(-10, 10, 0.2)
+    X1, X2 = np.meshgrid(x1, x2)
+    F = X1**2 + X2**2
+    # 或者使用cmap = plt.cm.Blues绘制蓝色系三维图
+    ax.plot_surface(X1, X2, F, rstride=1, cstride=1, cmap=plt.get_cmap('rainbow'))
+    plt.show()
+
+def main():
+    pop = 50
+    maxIter = 100
+    dim = 2
+    lb = -10 * np.ones(dim) # 下边界
+    ub = 10 * np.ones(dim) # 上边界
+
+    fobj = fun
+    GbestScore, GbestPosition, Curve = LibABC.ABC(pop, dim, lb, ub, maxIter, fobj)
+    print('The best fitness is: ', GbestScore)
+    print('The best solution is: ', GbestPosition)
+
+    # 绘制最优Fitness曲线
+    plt.plot(Curve, 'r-', lw=1)
+    plt.xlabel('Iteration', fontsize='medium')
+    plt.ylabel('Fitness', fontsize='medium')
+    plt.grid()
+    plt.title('ABC', fontsize='large')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+
---
+++ HeuristicAlgorithms/P2-ABC/Example2.py
@@ -0,0 +1,48 @@
+"""
+三杆设计
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import LibABC
+
+# 适应度函数
+def fun(X):
+    x1 = X[0]
+    x2 = X[1]
+    l = 100
+    P = 2
+    sigma = 2
+    # 约束条件判断
+    g1 = (np.sqrt(2)*x1+x2)*P / (np.sqrt(2)*x1**2+2*x1*x2)-sigma
+    g2 = x2*P / (np.sqrt(2)*x1**2+2*x1*x2) - sigma
+    g3 = P / (np.sqrt(2)*x2+x1)-sigma
+
+    if g1<=0 and g2<=0 and g3<=0:
+        # 满足约束条件，计算适应度值
+        fitness = (2*np.sqrt(2)*x1+x2)*l
+    else:
+        fitness = 1e32
+    return fitness
+
+def main():
+    pop = 30
+    maxIter = 100
+    dim = 2
+    lb = np.array([0.001, 0.001])
+    ub = np.array([1, 1])
+
+    fobj = fun
+    GbestScore, GbestPosition, Curve = LibABC.ABC(pop, dim, lb, ub, maxIter, fobj)
+    print('Fitness: ', GbestScore)
+    print('Optimal Solution: ', GbestPosition)
+
+    plt.figure()
+    plt.plot(Curve, 'r-', lw=1)
+    plt.xlabel('Iteration', fontsize='medium')
+    plt.ylabel('Fitness', fontsize='medium')
+    plt.grid()
+    plt.title('SMA', fontsize='large')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
